452fd56684c25d3d07fb1e6b8dae4dc2dc42ed6b
Gymoblig <matusondrejka3@gmail.com>
2025-04-27 15:27:42 +0200

    GG banger update fr sexy

--- ZADANIE 2/Zad2_manipulator.m
+++ ZADANIE 2/Zad2_manipulator.m
@@ -1,10 +1,54 @@
-m1 = 3;
-m2 = 3;
-l1 = 0.25;
-l2 = 0.25;
-B1 = 2;
-B2 = 2;
-g = 9.81;
+% Inicializácia parametrov systému
+m1 = 3;           % Hmotnosť 1. ramena [kg]
+m2 = 3;           % Hmotnosť 2. ramena [kg]
+l1 = 0.25;        % Dĺžka 1. ramena [m]
+l2 = 0.25;        % Dĺžka 2. ramena [m]
+B1 = 2;           % Tlmenie 1. kĺbu
+B2 = 2;           % Tlmenie 2. kĺbu
+g = 9.81;         % Gravitačné zrýchlenie [m/s^2]
+final_uhol = -pi/4; 
 
-q1 = 0;
-q2= 0;
+% Spustenie simulácie
+simOut = sim('Zad2manipulator.slx');
+out = simOut;
+
+
+% Polohová odozva ramena q1
+figure;
+plot(out.q1.time, out.q1.signals.values, 'LineWidth', 2);
+xlabel('Čas [s]');
+ylabel('Uhol q1 [rad]');
+title('Polohová odozva ramena q1');
+grid on;
+
+% Polohová odozva ramena q2
+figure;
+plot(out.q2.time, out.q2.signals.values, 'LineWidth', 2);
+xlabel('Čas [s]');
+ylabel('Uhol q2 [rad]');
+title('Polohová odozva ramena q2');
+grid on;
+
+% Spoločná polohová odozva ramien q1 a q2
+figure;
+hold on;
+plot(out.q1.time, out.q1.signals.values, 'LineWidth', 2);
+plot(out.q2.time, out.q2.signals.values, 'LineWidth', 2);
+xlabel('Čas [s]');
+ylabel('Uhol [rad]');
+title('Polohová odozva ramien q1 a q2');
+legend('q1', 'q2');
+grid on;
+hold off;
+
+% Krútiace momenty τ1 a τ2
+figure;
+hold on;
+plot(out.t1.time, out.t1.signals.values, 'LineWidth', 2);
+plot(out.t2.time, out.t2.signals.values, 'LineWidth', 2);
+xlabel('Čas [s]');
+ylabel('Krútiaci moment [Nm]');
+title('Krútiace momenty manipulátora');
+legend('τ1', 'τ2');
+grid on;
+hold off;
